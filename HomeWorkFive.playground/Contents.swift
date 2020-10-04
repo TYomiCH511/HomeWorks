7445062dc10595f6c6899f9246dc934fecb9a261
TYomiCH <timankov511@gmail.com>
2020-10-04 23:04:08 +0300

    Add home work

--- HomeWorkFive.playground/Contents.swift
+++ HomeWorkFive.playground/Contents.swift
@@ -6,8 +6,10 @@ protocol Summable { static func +(lhs: Self, rhs: Self) -> Self }
 extension Int: Summable {}
 extension Double: Summable {}
 extension Float: Summable {}
+extension String: Summable {}
 
 func add<T: Summable>(valueOne: T, valueTwo: T) -> T {
+   
   return valueOne + valueTwo
 }
 let floatValue: Float = 1.3
@@ -18,6 +20,13 @@ print(add(valueOne: 11, valueTwo: floatValue))
 
 //Task two
 
+func toArray<V, C>(someDictionary: [C: V]) -> [(C, V)] {
+    
+    return Array(someDictionary.map{($0.key, $0.value)})
+}
+
+let dict = toArray(someDictionary: [1:"f", 3:"4"])
+print(dict)
 
 
 
